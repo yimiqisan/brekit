7c0ac462ced1f14034f0bdad1fb60394c539d15b
yimiqisan <yimiqisan@gmail.com>
2012-12-26 20:39:17 +0800

    fix bug

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,30 +1,44 @@
-from fabric.api import env
+from fabric.api import run
 from fabric.contrib.project import rsync_project
 from fabric.decorators import hosts
 
-#env.hosts=['bre@192.168.11.25']
-#env.password=('192.168.11.25', 'bre@daydayup')
-env.passwords = {'root@192.168.3.150:3222': 'rootforbre'}
-
+LOCAL_DIR = '/home/zhiyong.liu/code/svn/bfd/ptech/rec/brewebconf/branches/branch_zhiyong.liu/src/brewebconf'
 
 @hosts('bre@192.168.11.25')
-def sync_breconf():
-    ldir = '/home/zhiyong.liu/code/svn/brewebconf'
+def conf():
+    ldir = LOCAL_DIR
     rdir = '/home/bre/pyenv'
-    edir = ['*.pyc', '*.log']
-    rsync_project(local_dir=ldir, remote_dir=rdir, exclude=edir, delete=True)
-
-@hosts('bre@192.168.11.25')
-def sync_rest():
-    ldir = '/home/zhiyong.liu/code/svn/UtilService'
-    rdir = '/home/bre/pyrestenv'
-    edir = ['*.pyc', '*.log']
+    edir = ['*.pyc', '*.log', '_project', '.svn']
     rsync_project(local_dir=ldir, remote_dir=rdir, exclude=edir, delete=True)
 
 @hosts('bre@192.168.3.150:3222')
 def conf2test():
-    ldir = '/home/zhiyong.liu/code/svn/brewebconf'
+    ldir = LOCAL_DIR
     rdir = '/opt/bre/pyenv'
-    edir = ['*.pyc', '*.log']
+    edir = ['*.pyc', '*.log', '_project', '.svn', 'uwsgi.pid']
     rsync_project(local_dir=ldir, remote_dir=rdir, exclude=edir, delete=True)
+    run('kill -1 `cat /opt/bre/pyenv/brewebconf/uwsgi.pid`')
 
+@hosts('bre@alg-1:3222')
+def conf2oversea():
+    ldir = LOCAL_DIR
+    rdir = '/opt/bre/pyenv'
+    edir = ['settings.py', '*.pyc', '*.log', '_project', 'uwsgi.*', '.svn', '.json']
+    rsync_project(local_dir=ldir, remote_dir=rdir, exclude=edir, delete=True)
+    run('cd /opt/bre/pyenv;fab conf2oversea')
+
+@hosts('bre@rec-1:3222')
+def conf2rec():
+    ldir = LOCAL_DIR
+    rdir = '/opt/bre/pyenv'
+    edir = ['settings.py', '*.pyc', '*.log', '_project', 'uwsgi.*', '.svn', '.json']
+    rsync_project(local_dir=ldir, remote_dir=rdir, exclude=edir, delete=True)
+    run('kill -1 `cat /opt/bre/pyenv/brewebconf/uwsgi.pid`')
+
+@hosts('bre@bfd5-rec1:3222')
+def conf2alg2oversea():
+    ldir = '/opt/bre/pyenv/brewebconf'
+    rdir = '/opt/bre/pyenv'
+    edir = []
+    rsync_project(local_dir=ldir, remote_dir=rdir, exclude=edir, delete=True)
+    run('kill -1 `cat /opt/bre/pyenv/brewebconf/uwsgi.pid`')
